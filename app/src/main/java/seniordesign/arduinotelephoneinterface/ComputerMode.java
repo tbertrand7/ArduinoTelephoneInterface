0d21f02f82bb7a63ef416691498efdef8fa792cb
Tom Bertrand <tpb18@pitt.edu>
2016-11-21 14:41:59 -0500

    Computer mode reads in SMS messages

--- app/src/main/java/seniordesign/arduinotelephoneinterface/ComputerMode.java
+++ app/src/main/java/seniordesign/arduinotelephoneinterface/ComputerMode.java
@@ -2,12 +2,66 @@ package seniordesign.arduinotelephoneinterface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.*;
+import android.telephony.gsm.SmsMessage;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.provider.Telephony.*;
 
 public class ComputerMode extends AppCompatActivity {
+
+    private TextView txtResponse;
+
+    BroadcastReceiver smsReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //---get the SMS message passed in---
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] msgs = null;
+            String str = "";
+            if (bundle != null)
+            {
+                //---retrieve the SMS message received---
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                msgs = new SmsMessage[pdus.length];
+                for (int i=0; i<msgs.length; i++){
+                    msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                    str += "SMS from " + msgs[i].getOriginatingAddress();
+                    str += ": ";
+                    str += msgs[i].getMessageBody().toString();
+                }
+                //---display the new SMS message---
+                //Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+                tvAppend(txtResponse, str);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_computer_mode);
+
+        txtResponse = (TextView) findViewById(R.id.txtArduinoResponse);
+
+        // Register a broadcast receiver
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.setPriority(999);
+        intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        //intentFilter.addDataScheme("sms");
+        //intentFilter.addDataAuthority("*", "6734");
+        registerReceiver(smsReceiver, intentFilter);
+    }
+
+    private void tvAppend(TextView tv, CharSequence text) {
+        final TextView ftv = tv;
+        final CharSequence ftext = text;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ftv.append(ftext);
+            }
+        });
     }
 }
